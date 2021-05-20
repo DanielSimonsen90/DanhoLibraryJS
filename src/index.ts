62d690b51ae5aa910a5bad931b99726a90bf6148
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-05-20 07:17:07 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,36 +14,43 @@ interface SimpleEvent {
     name: string,
     handler: EventHandler
 }
-interface IEventCollection {
-    [eventName: string]: EventHandler[];
-    listeners: Array<EventHandler>;
-}
 //#endregion
 
 //#region Classes
-class EventCollection implements IEventCollection {
+class EventCollection {
     constructor(...events: SimpleEvent[]) {
-        this.events = [...events];
+        this.events = new Map<string, EventHandler[]>();
+        events.forEach(({ name, handler }) => this.setEvent(name, handler));
     }
 
-    [eventName: string]: {
-        
+    private events: Map<string, EventHandler[]>;
+    private setEvent(name: string, handler: EventHandler) {
+        this.events.set(name, [...(this.events.has(name) ? this.events.get(name) : []), handler])
+        return this;
     }
-    listeners: EventHandler[];
-
-    private events: SimpleEvent[];
+    
+    public has(event: string) { return this.events.has(event); }
+    public get(event: string) { return this.events.get(event); }
+    public add(name: string, handler: EventHandler) { return this.setEvent(name, handler); }
+    public clear(name: string = 'all', handler?: EventHandler) {
+        if (name.toLowerCase() == 'all' && handler == null) return this.events.clear();
+        else if (handler == null) this.events.delete(name);
+        else if (name.toLowerCase() != 'all' && handler) this.events.map((v, k) => v.includes(handler) && k)
+    }
 }
 
 class EventEmitter {
-    constructor() {
-
+    constructor(...events: SimpleEvent[]) {
+        this.events = new EventCollection(...events);
     }
 
-    public events: IEventCollection = { listeners: new Array<EventHandler>() };
+    public events: EventCollection;
     
     public on(event: string, listener: EventHandler) {
-        if (!this.events[event]) this.events[event] = new Array<EventHandler>();
-        this.events[event].push(listener);
+        this.events.add(event, listener);
+    }
+    public removeListener(event: string, listener: EventHandler) {
+        this.events.clear()
     }
 }
 //#endregion
@@ -107,5 +114,11 @@ HTMLCollection.prototype.array = function() {
         result.push(this.item(i));
     }
     return result;
+}
+Map.prototype.array = function<K, V>() {
+    
+}
+Map.prototype.map = function<K, V, EndType>(callback: (value: V, key?: K, map?: Map<K, V>) => EndType) {
+    return this.toArray().map(callback);
 }
 //#endregion
