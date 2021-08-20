5b69f072efea7d00a3d92a37e8e932b7a59831af
DanielSimonsen90 <danielsimonsen90@gmail.com>
2021-08-20 10:17:10 +0200

    Compiled src

---
+++ dist/Extensions.d.ts
@@ -0,0 +1,21 @@
+import ElementOptions from "./Interfaces/ElementOptions";
+declare global {
+    interface Document {
+        createProperElement<K extends keyof HTMLElementTagNameMap>(tagName: K, options?: ElementOptions<K>): HTMLElementTagNameMap[K];
+    }
+    interface HTMLCollection {
+        array(): Element[];
+    }
+    interface Array<T> {
+        remove(item: T): this;
+    }
+    interface Map<K, V> {
+        array(): [K, V][];
+        map<EK, EV>(callback: (value: V, key?: K, index?: number, map?: this) => [EK, EV]): Map<EK, EV>;
+        filter(callback: (value: V, key?: K, index?: number, map?: Map<K, V>) => boolean): Map<K, V>;
+        keyArr(): Array<K>;
+        valueArr(): Array<V>;
+        find(callback: (value: V, key?: K, index?: number, map?: Map<K, V>) => boolean): [K, V];
+        includes(value: V, fromIndex?: number): boolean;
+    }
+}
