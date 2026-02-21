dba79197fd4c0dedecc3e1f152c87ccf37d2dc99
DanielSimonsen90 <danielsimonsen90@gmail.com>
2026-02-21 20:02:35 +0100

    Add AbortController to debounce

--- src/Utils/TimeUtils/debounce.util.ts
+++ src/Utils/TimeUtils/debounce.util.ts
@@ -1,5 +1,6 @@
 const DebounceManager = {
   timers: new Map<string, number>(),
+  abortListeners: new Map<string, () => void>(),
 }
 
 /**
@@ -9,31 +10,76 @@ const DebounceManager = {
  * @param debounceId A unique identifier for the debounce instance. Call with the same ID to reset the timer.
  * @param callback The callback to run after the delay has passed without the function being called again
  * @param delay The delay in milliseconds to wait before running the callback after the last call
- * @returns A promise that resolves to the result of the callback when it is eventually called
+ * @param signal Optional AbortSignal to cancel the debounced operation
+ * @returns A promise that resolves to the result of the callback when it is eventually called, or rejects if aborted
  */
-export function debounce<T>(debounceId: string, callback: () => T, delay: number) {
-  const { timers } = DebounceManager;
+export function debounce<T>(debounceId: string, callback: () => T, delay: number, signal?: AbortSignal) {
+  const { timers, abortListeners } = DebounceManager;
 
-  if (timers.has(debounceId)) clearTimeout(timers.get(debounceId));
+  // Clean up existing debounce with the same ID
+  if (timers.has(debounceId)) {
+    clearTimeout(timers.get(debounceId));
+    timers.delete(debounceId);
+    
+    const existingListener = abortListeners.get(debounceId);
+    if (existingListener && signal) {
+      signal.removeEventListener('abort', existingListener);
+    }
+    abortListeners.delete(debounceId);
+  }
 
-  return new Promise<T>((resolve) => {
+  const promise = new Promise<T>((resolve, reject) => {
+    // Check if already aborted
+    if (signal?.aborted) {
+      const error = new DOMException('Debounce aborted', 'AbortError');
+      reject(error);
+      return;
+    }
+
     const timeoutId = setTimeout(() => {
       timers.delete(debounceId);
+      abortListeners.delete(debounceId);
 
-      resolve(callback());
+      try {
+        resolve(callback());
+      } catch (error) {
+        reject(error);
+      }
     }, delay);
 
     timers.set(debounceId, timeoutId);
+
+    // Set up abort listener
+    if (signal) {
+      const abortListener = () => {
+        clearTimeout(timeoutId);
+        timers.delete(debounceId);
+        abortListeners.delete(debounceId);
+        
+        const error = new DOMException('Debounce aborted', 'AbortError');
+        reject(error);
+      };
+
+      signal.addEventListener('abort', abortListener, { once: true });
+      abortListeners.set(debounceId, abortListener);
+    }
   });
+
+  return promise;
 }
 
 /**
  * Wrap a callback in a debounce, allowing it to be called multiple times, but only run after the delay has passed without it being called again
  * @param callback The callback to run after the delay has passed without the function being called again
  * @param delay The delay in milliseconds to wait before running the callback after the last call
- * @returns A debounced version of the callback that can be called multiple times, but will only run after the delay has passed without it being called again
+ * @returns An object containing the debounced function, an AbortController to cancel operations, and the abort method for convenience
  */
 export function wrapInDebounce<T>(callback: (...args: T[]) => void, delay: number) {
-  const debounceId = Symbol().toString();
-  return (...args: T[]) => debounce(debounceId, () => callback(...args), delay);
+  const controller = new AbortController();
+  
+  return {
+    debounced: (...args: T[]) => debounce(Symbol().toString(), () => callback(...args), delay, controller.signal),
+    controller,
+    abort: () => controller.abort(),
+  };
 }
