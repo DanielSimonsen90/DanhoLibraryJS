869bd92a10e1c905c92de1d7d47ea3f1490fdcf5
DanielSimonsen90 <danielsimonsen90@gmail.com>
2026-02-21 19:45:45 +0100

    Fix ColorUtils

--- src/Utils/ColorUtils.ts
+++ src/Utils/ColorUtils.ts
@@ -3,26 +3,31 @@ export type Hex = `#${string}`;
 export type ColorType = 'hex' | 'rgb' | 'hsl';
 
 
-export function convert(value: string, from: ColorType, to: ColorType): string {
-  if (from === to) return value;
+export function convert(value: RGB, to: Exclude<ColorType, 'rgb'>): string;
+export function convert(value: string, from: Exclude<ColorType, 'rgb'>, to: 'rgb'): RGB;
+export function convert(value: string | RGB, fromOrTo: ColorType, to?: ColorType): string | RGB {
+  const from = typeof value === 'string' ? fromOrTo : 'rgb';
+  const target = typeof value === 'string' ? to : fromOrTo;
+
+  if (from === target) return value as string;
 
   switch (from) {
     case 'hsl': {
-      switch (to) {
-        case 'hex': return hslToHex(value);
-        case 'rgb': return hexToRgb(hslToHex(value)).join(', ');
+      switch (target) {
+        case 'hex': return hslToHex(value as string);
+        case 'rgb': return hexToRgb(hslToHex(value as string)) as RGB;
       }
     }
     case 'rgb': {
-      switch (to) {
-        case 'hex': return rgbToHex(hexToRgb(value as Hex));
-        case 'hsl': return hslToHex(rgbToHex(hexToRgb(value as Hex)));
+      switch (target) {
+        case 'hex': return hslToHex(rgbToHsl(value as RGB))
+        case 'hsl': return rgbToHsl(value as RGB);
       }
     }
     case 'hex': {
-      switch (to) {
-        case 'rgb': return hexToRgb(value as Hex).join(', ');
-        case 'hsl': return hslToHex(value as Hex);
+      switch (target) {
+        case 'rgb': return hexToRgb(value as Hex) as RGB;
+        case 'hsl': return rgbToHsl(hexToRgb(value as Hex));
       }
     }
     default: return value;
@@ -75,7 +80,23 @@ function hexToRgb(hex: Hex): RGB {
   return [r, g, b];
 }
 
-function rgbToHex(rgb: RGB): Hex {
-  const [r, g, b] = rgb;
-  return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+function rgbToHsl(rgb: RGB): string {
+  let [r, g, b] = rgb.map(x => x / 255);
+  const max = Math.max(r, g, b), min = Math.min(r, g, b);
+  let h = 0, s = 0, l = (max + min) / 2;
+
+  if (max !== min) {
+    const d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return `hsl(${Math.round(h * 360)}, ${Math.round(s * 100)}%, ${Math.round(l * 100)}%)`;
 }
